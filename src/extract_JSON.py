f01e776707fd64a69d833eb5999911812321f05c
satan53 <1394170527@qq.com>
2023-07-24 16:35:53 +0800

    JSON不再自动复制key到value

--- src/extract_JSON.py
+++ src/extract_JSON.py
@@ -27,18 +27,18 @@ def parseImp(content, listCtrl, dealOnce):
 		if lineData == '': continue #空白行
 		if re.match(r'\s*[\[\]\{\}]', lineData): continue #括号
 		# 确认需要匹配的数据段
-		ret = re.match(r'\s*"(.+?)":\s*"(.*?)"', lineData)
+		ret = re.search(r'"([^:]*)"[ ,]*$', lineData)
 		if ret:
-			value = ret.group(2)
-			var.searchStart = ret.start(2)
-			var.searchEnd = ret.end(2)
+			value = ret.group(1)
+			var.searchStart = ret.start(1)
+			var.searchEnd = ret.end(1)
 			# value为空则复制key到value
-			if value == '':
-				key = ret.group(1)
-				strNew = content[contentIndex][:var.searchStart] + key + content[contentIndex][var.searchEnd:]
-				content[contentIndex] = strNew
-				lineData = content[contentIndex][:-1]
-				var.searchEnd += len(key)
+			# if value == '':
+			# 	key = ret.group(1)
+			# 	strNew = content[contentIndex][:var.searchStart] + key + content[contentIndex][var.searchEnd:]
+			# 	content[contentIndex] = strNew
+			# 	lineData = content[contentIndex][:-1]
+			# 	var.searchEnd += len(key)
 		else:
 			print('\033[33m查找键值对失败\033[0m', lineData)
 			continue
