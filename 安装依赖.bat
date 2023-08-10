2f69e8f2038494aebc1d86832197d27026370f5b
satan53 <1394170527@qq.com>
2023-08-10 11:14:39 +0800

    添加：安装依赖.bat

---
+++ 安装依赖.bat
@@ -0,0 +1,5 @@
+@rem 设置国内镜像
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+@rem 安装依赖
+pip install pyqt5
+@pause
--- 运行.bat
+++ 运行.bat
@@ -1,3 +1,2 @@
-@rem pip install pyqt5 -i https://mirrors.aliyun.com/pypi/simple/
 python run.py
 @pause
