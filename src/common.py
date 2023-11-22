d8a7690f454a00a78239bc088d1c52a49176ff19
satan53 <1394170527@qq.com>
2023-11-22 22:50:32 +0800

    增加checkJIS在cp932下的范围
    第一字节增加FA,FB,FC

--- src/common.py
+++ src/common.py
@@ -52,8 +52,11 @@ def printDebug(tip, *args):
 #判断是否是日文
 def isShiftJis(byte1, byte2):
     # 检查字节范围
-    if (byte1 >= 0x81 and byte1 <= 0x9F) or (byte1 >= 0xE0 and byte1 <= 0xEF):
+    if (byte1 >= 0x81 and byte1 <= 0x9F) or (byte1 >= 0xE0 and byte1 <= 0xEF) or (byte1 >= 0xFA and byte1 <= 0xFB):
         if (byte2 >= 0x40 and byte2 <= 0x7E) or (byte2 >= 0x80 and byte2 <= 0xFC):
+            return 2
+    elif byte1 == 0xFC:
+        if (byte2 >= 0x40 and byte2 <= 0x4B):
             return 2
     return 0
 
