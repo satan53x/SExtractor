312a2305f8a0dd6135d318519af0274174ec30f9
satan53 <1394170527@qq.com>
2024-03-02 12:45:08 +0800

    JSON引擎添加extraData
    extraData=^reg$用于跳过匹配的key

--- src/extract_JSON.py
+++ src/extract_JSON.py
@@ -1,5 +1,5 @@
 from common import *
-from extract_TXT import ParseVar, GetRegList, searchLine, initParseVar
+from extract_TXT import ParseVar, GetRegList, searchLine, initParseVar, dealLastCtrl
 
 copyKeyToValue = True
 
@@ -10,6 +10,11 @@ def parseImp(content, listCtrl, dealOnce):
 	initParseVar(var)
 	#print(len(content))
 	ExVar.indent = 2
+	skipKeyReg = None
+	if ExVar.extraData:
+		skipKeyReg = re.compile(ExVar.extraData) #跳过key
+	checkLast = ExVar.structure.startswith('para')
+	lastCtrl = None
 	if isinstance(content, dict):
 		#字典: 子项为字符串key:value
 		if copyKeyToValue:
@@ -25,7 +30,9 @@ def parseImp(content, listCtrl, dealOnce):
 			if lineData == '': continue #空白行
 			var.contentIndex = [i, None]
 			var.lineData = lineData
-			searchLine(var)
+			ctrls = searchLine(var)
+			if checkLast:
+				lastCtrl = dealLastCtrl(lastCtrl, ctrls, i)
 	else:
 		#列表
 		if isinstance(content[0], dict):
@@ -33,19 +40,24 @@ def parseImp(content, listCtrl, dealOnce):
 			for i in range(len(content)):
 				if i < ExVar.startline: continue 
 				for j, value in content[i].items(): #j可以是key值
+					if skipKeyReg and skipKeyReg.search(j):
+						continue
 					lineData = value
 					if lineData == '': continue #空白行
 					var.contentIndex = [i, j]
 					var.lineData = lineData
 					ctrls = searchLine(var)
-					if ExVar.keepFormat and ctrls and len(ctrls) > 0:
+					if checkLast:
+						lastCtrl = dealLastCtrl(lastCtrl, ctrls, i)
+					elif ExVar.keepFormat:
 						#保持格式
-						ctrl = ctrls[-1]
-						if j == 'name':
-							ctrl['name'] = True
-						elif j == 'message':
-							if 'unfinish' in ctrl:
-								del ctrl['unfinish']
+						if ctrls and len(ctrls) > 0:
+							ctrl = ctrls[-1]
+							if j == 'name':
+								ctrl['name'] = True
+							elif j == 'message':
+								if 'unfinish' in ctrl:
+									del ctrl['unfinish']
 		elif isinstance(content[0], str):
 			#子项为字符串
 			for i in range(len(content)):
@@ -54,7 +66,9 @@ def parseImp(content, listCtrl, dealOnce):
 				if lineData == '': continue #空白行
 				var.contentIndex = [i, None]
 				var.lineData = lineData
-				searchLine(var)
+				ctrls = searchLine(var)
+				if checkLast:
+					lastCtrl = dealLastCtrl(lastCtrl, ctrls, i)
 
 # -----------------------------------
 def replaceOnceImp(content, lCtrl, lTrans):
