cd9dd5ef182f3db8d7d5ae2b198a8555cce532cc
satan53 <1394170527@qq.com>
2024-03-13 18:50:42 +0800

    添加Cyberworks旧版本支持
    特征为a0未加密，显示JIS编码明文

--- src/extract_Cyberworks.py
+++ src/extract_Cyberworks.py
@@ -5,10 +5,19 @@ from extract_BIN import replaceOnceImp as replaceOnceImpBIN
 from extract_BIN import parseImp as parseImpBIN
 
 headerList = []
+decrypt = True #进行加密和解密
+existTextLen = True #存在文本长度字节
 
 def initExtra():
+	global decrypt, existTextLen
+	#文本长度字节
+	lst = ExVar.extraData.split(',')
+	existTextLen = 'noTextLen' not in lst
+	decrypt = existTextLen #有长度字节才会进行加解密
+	if ExVar.decrypt == '0':
+		decrypt = False #强制指定
 	#默认编码
-	if ExVar.extraData.startswith('readJIS'): return
+	if 'readJIS' in lst: return
 	ExVar.OldEncodeName = 'utf-16-le'
 	ExVar.NewEncodeName = 'utf-16-le'
 	ExVar.pureText = True #强制使用纯文本模式，不然正则pattern编码会有问题
@@ -29,18 +38,22 @@ def replaceEndImp(content):
 		if header['type'] == 'S': #文本
 			#pre
 			data.extend(header['pre'])
-			#文本的字节长度
-			textLen = len(lineData)
-			bs = int2bytes(textLen)
-			data.extend(bs)
-			#xor密钥
-			if textLen < 0x100:
-				xorKey = bs[0:1]
-			else:
-				xorKey = None
-				printWarningGreen('文本长度超过0x100', ExVar.filename, lineData)
+			if existTextLen:
+				#文本的字节长度
+				textLen = len(lineData)
+				bs = int2bytes(textLen)
+				data.extend(bs)
+				#xor密钥
+				if textLen < 0x100:
+					xorKey = bs[0:1]
+				else:
+					xorKey = None
+					printWarningGreen('文本长度超过0x100', ExVar.filename, lineData)
 			#文本加密
-			bs = xorBytes(lineData, xorKey)
+			if decrypt:
+				bs = xorBytes(lineData, xorKey)
+			else:
+				bs = lineData
 			data.extend(bs)
 		elif header['type'] == 'T': #文本
 			#pre
@@ -52,7 +65,10 @@ def replaceEndImp(content):
 			#xor密钥
 			xorKey = bs[0:2]
 			#文本加密
-			bs = xorBytes(lineData, xorKey)
+			if decrypt:
+				bs = xorBytes(lineData, xorKey)
+			else:
+				bs = lineData
 			data.extend(bs)
 		#post
 		if 'post' in header:
@@ -84,19 +100,23 @@ def readFileDataImp(fileOld, contentSeparate):
 			pre = bytearray(data[pos:pos+1])
 			header['pre'] = pre
 			pos += 1
-			#文本的字节长度
-			textLen = readInt(data, pos)
-			header['textLen'] = textLen
-			#xor密钥
-			if textLen < 0x100:
-				xorKey = data[pos:pos+1]
+			if existTextLen:
+				#文本的字节长度
+				textLen = readInt(data, pos)
+				header['textLen'] = textLen
+				#xor密钥
+				if textLen < 0x100:
+					xorKey = data[pos:pos+1]
+				else:
+					xorKey = None
+					printWarningGreen('文本长度超过0x100', ExVar.filename, pos)
+				pos += 4
 			else:
-				xorKey = None
-				printWarningGreen('文本长度超过0x100', ExVar.filename, pos)
-			pos += 4
+				textLen = length-1 #文本使用块长-1
 			#文本解密
 			bs = data[pos:pos+textLen] #字节长度
-			bs = xorBytes(bs, xorKey)
+			if decrypt:
+				bs = xorBytes(bs, xorKey)
 			content.append(bs)
 			pos += textLen
 			#post
@@ -117,7 +137,8 @@ def readFileDataImp(fileOld, contentSeparate):
 			pos += 4
 			#文本解密
 			bs = data[pos:pos+textLen*2] #Unicode长度
-			bs = xorBytes(bs, xorKey)
+			if decrypt:
+				bs = xorBytes(bs, xorKey)
 			content.append(bs)
 			pos += textLen*2
 			#post
