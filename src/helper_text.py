c84faa81cac29381e2308b745524bdeb29e8dcdd
sakura4214 <113588801+sakura4214@users.noreply.github.com>
2024-04-27 13:17:14 +0800

    Revert "内部字典更改为可以一个原文对应多个译文"
    This reverts commit 2edb0dfc56bab0f68b8743e07bd68fcf372dfafb.

--- src/helper_text.py
+++ src/helper_text.py
@@ -7,7 +7,7 @@ __all__ = ['splitToTransDic', 'splitToTransDicAuto',
 		'generateJisList', 'generateTunnelJis', 'generateTunnelJisMap',
 		'generateSubsDic', 'generateSubsJis', 'generateSubsConfig',
 		'writeSubsConfig',
-		'replaceValue', 'keepFirstTrans',
+		'replaceValue',
 		'getBytes', 'OldEncodeName'
 ]
 
@@ -126,11 +126,13 @@ def splitToTransDic(orig, trans):
 		msgTrans = ExVar.addSpace
 		if j<len(listMsgTrans) and listMsgTrans[j] != '':
 			msgTrans = listMsgTrans[j]
-		if msgOrig not in ExVar.transDic:
-			ExVar.transDic[msgOrig] = []
-		ExVar.transDic[msgOrig].append(msgTrans)
-		if len(msgTrans) > ExVar.maxCountPerLine:
-			printWarning('长度超过设置阈值', msgTrans)
+		if  msgOrig not in ExVar.transDic or \
+			ExVar.transDic[msgOrig] == '' or \
+			ExVar.transDic[msgOrig] == '　' or \
+			ExVar.transDic[msgOrig] == ' ':
+			if len(msgTrans) > ExVar.maxCountPerLine:
+				printWarning('长度超过设置阈值', msgTrans)
+			ExVar.transDic[msgOrig] = msgTrans
 
 #自动重新分割
 def splitToTransDicAuto(orig, trans):
@@ -140,9 +142,7 @@ def splitToTransDicAuto(orig, trans):
 		msgTrans = listMsgTrans[j]
 		if len(msgTrans) > ExVar.maxCountPerLine:
 			printWarning('长度超过设置阈值', msgTrans)
-		if msgOrig not in ExVar.transDic:
-			ExVar.transDic[msgOrig] = []
-		ExVar.transDic[msgOrig].append(msgTrans)
+		ExVar.transDic[msgOrig] = msgTrans
 
 #重新分割
 def redistributeTrans(orig:str, trans:str):
@@ -382,17 +382,8 @@ def writeSubsConfig(filepath=''):
 def replaceValue(transDic, replaceDic, useRE=True):
 	for orig, trans in transDic.items():
 		for old, new in replaceDic.items():
-			for i, t in enumerate(trans):
-				if useRE:
-					trans[i] = re.sub(old, new, t)
-				else:
-					trans[i] = t.replace(old, new)
-
-#只保留第一个，用于输出
-def keepFirstTrans(transDic):
-	if not isinstance(transDic, dict):
-		return transDic
-	newDic = {}
-	for orig, trans in transDic.items():
-		newDic[orig] = trans[0]
-	return newDic
+			if useRE:
+				trans = re.sub(old, new, trans)
+			else:
+				trans = trans.replace(old, new)
+			transDic[orig] = trans
