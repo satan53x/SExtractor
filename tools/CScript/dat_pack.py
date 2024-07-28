f07dc178d1a4f48a967cb6d4216f778bf8a2d694
satan53 <1394170527@qq.com>
2024-07-28 13:09:33 +0800

    CScript dat_pack改为日文环境系统排序

--- tools/CScript/dat_pack.py
+++ tools/CScript/dat_pack.py
@@ -1,9 +1,13 @@
 # ------------------------------------------------------------
 # https://github.com/satan53x/SExtractor/tree/main/tools/CScript
+# 依赖模块：pip install natsort
 # ------------------------------------------------------------
+import locale
+import re
 import sys
 import os
 from tkinter import filedialog
+from natsort import ns, natsorted, os_sorted
 DefaultPath = r''
 PackName = '../scr.dat'
 NameLen = 0x2C #根据原包头部的单个名字长度进行设定，常见值为：0x2C, 0x44, 0x64
@@ -54,6 +58,47 @@ def write():
 	fileNew.close()
 	print(f'Write done: {name}')
 
+#广度优先，系统排序
+file_list = []
+root_path = ''
+def listFiles(start_path):
+	global root_path
+	root_path = start_path
+	file_list.clear()
+	locale.setlocale(locale.LC_ALL, 'ja_JP')
+	listDir('')
+	return file_list
+
+def listDir(middle_path):
+	dir_path = os.path.join(root_path, middle_path)
+	#所有文件
+	files = [filename for filename in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, filename))]
+	files = sortWindows(files)
+	for filename in files:
+		relative_path = os.path.join(middle_path, filename)
+		file_list.append(relative_path)
+	#所有文件夹
+	files = [filename for filename in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, filename))]
+	files = sortWindows(files)
+	for dirname in files:
+		relative_path = os.path.join(middle_path, dirname)
+		listDir(relative_path)
+
+#按windows顺序
+def sortWindows(filenames):
+	#分出ascii开头
+	filesAscii = []
+	files = []
+	for filename in filenames:
+		if re.match(r'^[ -~]', filename):
+			filesAscii.append(filename)
+		else:
+			files.append(filename)
+	#排序
+	filesAscii = os_sorted(filesAscii) #系统排序
+	files = natsorted(files, alg=ns.LOCALE) #local排序
+	return filesAscii + files
+	
 def main():
 	path = DefaultPath
 	if path:
@@ -68,15 +113,7 @@ def main():
 	if os.path.isdir(path):
 		dirpath = path
 		print('工作目录', dirpath)
-		for root, dirs, files in os.walk(dirpath):
-			for file in files:
-				filepath = os.path.join(root, file)
-				#print(filepath)
-				if filepath.startswith(dirpath):
-					filename = filepath[len(dirpath):].lstrip(os.path.sep)
-				else:
-					print('绝对路径不匹配:', filepath, dirpath)
-					continue
-				filenameList.append(filename)
+		files = listFiles(path)
+		filenameList.extend(files)
 		pack()
 main()
