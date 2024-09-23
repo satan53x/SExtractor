47d3b08958cd8c73ff7605f078245a95ef56b9c2
satan53 <1394170527@qq.com>
2024-09-24 02:51:20 +0800

    update

---
+++ tools/Unity/gameframework_datatable.py
@@ -0,0 +1,263 @@
+# ------------------------------------------------------------
+# https://github.com/satan53x/SExtractor/tree/main/tools/Unity
+# 用于GameFramework的DataTable解压
+# table结构为开发者自定义，不通用，需要自行添加RowTypeDic
+# read_7bit_encoded_int()函数也可能需要自行修改
+# ------------------------------------------------------------
+import csv
+import io
+import json
+import sys
+import os
+import pandas as pd
+from tkinter import filedialog
+DefaultPath = ''
+EncodeName = 'utf-8'
+BIN = 'bytes'
+TXT = 'json' #支持格式：csv, json
+IfBin2Txt = False #True: BIN->TXT ; False: TXT->BIN
+
+RowTypeDic = {
+	'tbachievement': [ 
+		'str', 'int', 'int', 'str', 'int', 'str', 'str', 'str', 
+		'dic', 'dic', 'int' 
+	],
+	'tbuitextlanguage': [ 
+		'str', 'dic' 
+	],
+}
+
+# ------------------------------------------------------------
+#var
+dirpath = ''
+filename = '' 
+content:pd.DataFrame = None
+# ------------------------------------------------------------
+def decompress():
+	global content
+	data = read()
+	if not data:
+		return
+	reader = io.BytesIO(data)
+	#处理
+	rowCount = read_7bit_encoded_int(reader, 7)
+	header = []
+	rowConf = RowTypeDic[filename]
+	for i, colType in enumerate(rowConf):
+		header.append(f'col{i:02}')
+	content = pd.DataFrame(columns=header)
+	for i in range(rowCount):
+		content.loc[i] = read_row(reader, rowConf)
+	#生成
+	if reader.tell() < len(data):
+		print('Doesn\'t reach the end of file:', filename)
+	write()
+
+def compress():
+	global content
+	data = read()
+	if (not isinstance(data, pd.DataFrame) and not data) or data.empty:
+		return
+	content = data
+	writer = io.BytesIO()
+	#处理
+	rowCount = len(content)
+	write_7bit_encoded_int(rowCount, writer)
+	rowConf = RowTypeDic[filename]
+	for i in range(rowCount):
+		try:
+			wrtite_row(content.loc[i], writer, rowConf)
+		except ValueError as e:
+			print('ValueError:', filename, i)
+		except Exception as e:
+			raise e
+	#生成
+	content = writer.getvalue()
+	write()
+
+# ------------------------------------------------------------
+def read_row(reader, rowConf):
+	row = []
+	for i, colType in enumerate(rowConf):
+		if colType == 'str':
+			data = read_string(reader)
+		elif colType == 'dic':
+			data = read_dic(reader)
+			if TXT == 'csv':
+				#不支持嵌套的txt格式
+				data = json.dumps(data, ensure_ascii=False)
+		else:
+			data = read_7bit_encoded_int(reader)
+		row.append(data)
+	return row
+
+def read_7bit_encoded_int(reader, max=7):
+	value = 0
+	shift = 0
+	while True:
+		b = reader.read(1)[0]
+		if shift >= max or (b & 0x80) == 0:
+			value <<= 8
+			value += b
+			break
+		value <<= 7
+		value += b & 0x7f
+		shift += 7
+	return value
+
+def read_string(reader):
+	length = read_7bit_encoded_int(reader)
+	if length == 0:
+		return ""
+	bs =  reader.read(length)
+	s = bs.decode(EncodeName)
+	return s
+
+def read_dic(reader):
+	dic_count = read_7bit_encoded_int(reader)
+	if dic_count == 0:
+		return {}
+	dic = {}
+	for i in range(dic_count):
+		key = read_7bit_encoded_int(reader)
+		val = read_string(reader)
+		dic[key] = val
+	return dic
+
+# ------------------------------------------------------------
+def wrtite_row(row, writer, rowConf):
+	for i, colType in enumerate(rowConf):
+		data = row[i]
+		if colType == 'str':
+			write_string(data, writer)
+		elif colType == 'dic':
+			if TXT == 'csv':
+				#不支持嵌套的txt格式
+				data = json.loads(data)
+			write_dic(data, writer)
+		else:
+			write_7bit_encoded_int(data, writer)
+
+
+def write_7bit_encoded_int(value, writer, max=7):
+	ba = []
+	shift = 8
+	b = int(value & 0xFF)
+	ba.append(b)
+	value >>= 8
+	while value > 0:
+		ba.append(int(value & 0x7F) + 0x80)
+		value >>= 7
+		shift += 7
+		if shift >= max:
+			if value > 0:
+				print("Error: too long number for compression")
+				raise ValueError("too long number for compression")
+			break
+	if len(ba) == 1 and ba[0] > 0x7F:
+		ba.append(0x80)
+	for i in reversed(range(len(ba))):
+		writer.write(ba[i].to_bytes(1, byteorder="big"))
+
+def write_string(value, writer):
+	if pd.isnull(value):
+		write_7bit_encoded_int(0, writer)
+		return
+	bs = value.encode(EncodeName)
+	length = len(bs)
+	write_7bit_encoded_int(length, writer)
+	writer.write(bs)
+
+def write_dic(dic, writer):
+	dic_count = len(dic.items())
+	write_7bit_encoded_int(dic_count, writer)
+	for key, value in dic.items():
+		try:
+			key = int(key) #key可能是字符串
+		except Exception:
+			pass
+		write_7bit_encoded_int(key, writer)
+		write_string(value, writer)
+
+# ------------------------------------------------------------
+def read():
+	#print(filename)
+	if filename not in RowTypeDic:
+		return None
+	if IfBin2Txt:
+		filepath = os.path.join(dirpath, BIN, filename+'.'+BIN)
+		if not os.path.exists(filepath):
+			return None
+		fileOld = open(filepath, 'rb')
+		data = fileOld.read()
+		fileOld.close()
+	else:
+		filepath = os.path.join(dirpath, TXT, filename+'.'+TXT)
+		if not os.path.exists(filepath):
+			return None
+		fileOld = open(filepath, 'rb')
+		if TXT == 'csv':
+			data = pd.read_csv(fileOld, header=None, sep='\t', quoting=csv.QUOTE_MINIMAL)
+		elif TXT == 'json':
+			data = pd.read_json(fileOld, orient ='values')
+		fileOld.close()
+	return data
+
+def write():
+	global content
+	name = filename
+	if IfBin2Txt:
+		path = os.path.join(dirpath, TXT)
+		if not os.path.exists(path):
+			os.makedirs(path)
+		filepath = os.path.join(path, name+'.'+TXT)
+		fileNew = open(filepath, 'w', encoding=EncodeName, newline='')
+		if TXT == 'csv':
+			content = content.to_csv(None, header=None, index=False, sep='\t')
+			fileNew.write(content)
+		elif TXT == 'json':
+			content = content.to_json(orient ='values', indent=2, force_ascii=False)
+			fileNew.write(content)
+		else:
+			print('Unsupported output format:', TXT)
+	else:
+		path = os.path.join(dirpath, BIN+'.new')
+		if not os.path.exists(path):
+			os.makedirs(path)
+		filepath = os.path.join(path, name+'.'+BIN)
+		fileNew = open(filepath, 'wb')
+		fileNew.write(content)
+	fileNew.close()
+	print(f'Write done: {name}')
+
+def main():
+	path = DefaultPath
+	if path:
+		pass
+	elif len(sys.argv) < 2:
+		path = filedialog.askdirectory(initialdir=path)
+	else:
+		path = sys.argv[1]
+	#print(path)
+	global dirpath, filename
+	if os.path.isdir(path):
+		dirpath = path
+		#print(dirpath)
+		if IfBin2Txt:
+			binpath = os.path.join(dirpath, BIN)
+			for name in os.listdir(binpath):
+				#解压
+				if not name.endswith('.'+BIN):
+					continue
+				filename = name[:-len(BIN)-1]
+				decompress()
+		else:
+			txtpath = os.path.join(dirpath, TXT)
+			for name in os.listdir(txtpath):
+				#压缩
+				if not name.endswith('.'+TXT):
+					continue
+				filename = name[:-len(TXT)-1]
+				compress()
+
+main()
